8b65478b74b2540e3158338b6287092fb2b474b5
moonshay76 <moonshay76@gmail.com>
2020-02-10 08:37:59 -0500

    update #10

--- js/topicJ.js
+++ js/topicJ.js
@@ -7,34 +7,47 @@ var cOne = document.getElementById("picture1");
 var cFir = cOne.getContext('2d');
 
 // draw a square
-cFir.fillStyle = "#ff0000";
-cFir.fillRect(137.5, 62.5, 25, 25);
+cFir.fillStyle = "#ffe4b5";
+cFir.fillRect(130.5, 55.5, 40, 40);
 
-// add a border to the square
-cFir.beginPath();
-cFir.moveTo(180, 50);
-cFir.lineTo(120, 50);
-cFir.lineTo(120, 100);
-cFir.lineTo(180, 100);
-cFir.lineTo(180, 50);
-cFir.strokeStyle = "#ff1493";
-cFir.stroke();
 
-// draw some circles
+// draw a square border
 cFir.beginPath();
-cFir.arc(95, 75, 50, 0, Math.PI * 2, false);
+cFir.moveTo(190, 40);
+cFir.lineTo(110, 40);
+cFir.lineTo(110, 110);
+cFir.lineTo(190, 110);
+cFir.lineTo(190, 40);
 cFir.strokeStyle = "#6f00ff";
 cFir.stroke();
 
+
+// add bigger empty squares
 cFir.beginPath();
-cFir.arc(205, 75, 50, 0, Math.PI * 2, false);
-cFir.strokeStyle = "#6f00ff";
+cFir.moveTo(140, 30);
+cFir.lineTo(50, 30);
+cFir.lineTo(50, 120);
+cFir.lineTo(140, 120);
+cFir.lineTo(140, 30);
+cFir.strokeStyle = "#d70a53";
 cFir.stroke();
 
 cFir.beginPath();
-cFir.arc(150, 75, 50, 0, Math.PI * 2, false);
-cFir.strokeStyle = "#6f00ff";
+cFir.moveTo(160, 30);
+cFir.lineTo(260, 30);
+cFir.lineTo(260, 120);
+cFir.lineTo(160, 120);
+cFir.lineTo(160, 30);
+cFir.strokeStyle = "#d70a53";
 cFir.stroke();
+
+
+//draw a circle around the little square
+cFir.beginPath();
+cFir.arc(150, 75, 70, 0, Math.PI * 2, false);
+cFir.strokeStyle = "#696969";
+cFir.stroke();
+
 
 
 // target the second canvas
@@ -43,4 +56,4 @@ var cTwo = document.getElementById("picture2");
 // set the context for canvas #2
 var cSec = cTwo.getContext('2d');
 cSec.font = "45px Apple Chancery, cursive";
-cSec.strokeText("Jamie", 85, 80);
+cSec.fillText("Happy Birthday", 30, 110);
