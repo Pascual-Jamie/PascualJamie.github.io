aec78e3210ae020037b65e5918bf3d25b5ce6d5e
moonshay76 <moonshay76@gmail.com>
2020-02-28 08:20:22 -0500

    last update topic 4

--- js/topicD.js
+++ js/topicD.js
@@ -14,7 +14,7 @@ btn.addEventListener('click', function () {
     var rqt = new XMLHttpRequest();
 
     // establish the connection
-    rqt.open('GET', 'https://api.weatherbit.io/v2.0/current?city=' + city + '&key=6e662e8988cd4c1fa9d3d9e9ccefa3ea', true);
+    rqt.open('GET', 'https://api.weatherbit.io/v2.0/current?city=' + city + '&units=I&key=6e662e8988cd4c1fa9d3d9e9ccefa3ea', true);
 
     rqt.send();
     // if the request is successfully completed, then go ahead
